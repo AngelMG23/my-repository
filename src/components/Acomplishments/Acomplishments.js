3d5d5af20b88cbf59304a5bf4b764ddf12acbb5d
AngelMG23 <r10mec8tronic7@gmail.com>
2022-05-20 18:00:39 -0500

    commit1

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -10,19 +10,23 @@ import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 const data = [
   {
     number: 2014,
-    text: "Baccalauréat Maintenance des Equipements Industriels",
+    text: "I managed the 3rd place in the Physics Olympics in Mexico State",
   },
-  { number: 2021, text: "Built 10 web and mobile applications in 10 weeks" },
+  { number: 2015, text: "I managed 2nd place in the Physics Olympics in Mexico State" },
   {
-    number: 2022,
-    text: "Titre professionnel Chef de projet de développement d'applications web et mobile (RNCP 6 - Niveau Bac +3/4)",
+    number: 2020,
+    text: "Was a TeamThor member within Mechanical team where was managed the 5th place in the CanSat Competition 2020",
   },
-  { number: 2022, text: "Built my first responsive website (this one)" },
+  { number: 2021, text: "I realized my thesis work to obtain the Mechatronics engineer degree" },
+  {
+    number: 2021,
+    text: "In December I finished my social service writing a research project at Centro de Desarrollo Espacial IPN"
+  }
 ];
 
 const Acomplishments = () => (
   <Section>
-    <SectionTitle>Personal Acomplishments</SectionTitle>
+    <SectionTitle>Personal Achievents</SectionTitle>
     <Boxes>
       {data.map((card, index) => (
         <Box key={index}>
