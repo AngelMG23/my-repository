4efb6d51f1d93733f8d57b65a09bfe46a8a1849b
AngelMG23 <r10mec8tronic7@gmail.com>
2022-05-22 00:09:18 -0500

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation"
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
 import Technologies from "../components/Technologies/Technologies";
+import Education from "../components/Education/Education";
 import Timeline from "../components/TimeLine/TimeLine";
 import { Layout } from "../layout/Layout";
 import { Section } from "../styles/GlobalComponents";
@@ -14,6 +15,7 @@ const Home = () => {
         <Hero />
         <BgAnimation />
       </Section>
+      <Education />
       <Technologies />
       <Timeline />
       <Acomplishments />
